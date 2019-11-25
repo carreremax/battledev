5ae4ed7be809737f3c5cc3708a7b10f147248320
maxime <maxime.carrere@eurogiciel.fr>
2019-11-25 14:34:51 +0100

    add 2019

--- src/exercices/meilleurdev_2019.py
+++ src/exercices/meilleurdev_2019.py
@@ -9,3 +9,18 @@ for line in sys.stdin:
 
 ages = [int(j) for j in lines[1].split() if 5 <= int(j) <= 9]
 print(len(ages))
+
+# Exercice 2
+import sys
+
+lines = []
+for line in sys.stdin:
+    lines.append(line.rstrip('\n'))
+
+nb_traj = 0
+for l in lines[1:]:
+    nb_traj += (int(l) - 1) // 10 + 1 # pour éviter que 10 donne 2, on fait -1, ça donne 9, puis rajoute 1 au résultat
+
+print(nb_traj)
+
+# Exercice 3
